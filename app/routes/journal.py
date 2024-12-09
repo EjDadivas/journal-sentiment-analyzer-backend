ddc94fb3f100b6181ebbfe6f30ca794a35b25da3
ashcbrd <cabardoash@gmail.com>
2024-12-10 05:33:14 +0800

    add category

--- app/routes/journal.py
+++ app/routes/journal.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Body, HTTPException, status
 from fastapi.responses import Response
-# from fastapi.encoders import jsonable_encoder
 from typing import List
 import json
 from bson import ObjectId, json_util
@@ -10,12 +9,31 @@ from app.database import journal_collection, student_collection
 from app.services.sentiment_analysis import sentiment_analysis
 from datetime import datetime
 from collections import defaultdict
+import logging
 
 class JSONEncoder(json.JSONEncoder):
     def default(self, o):   
         return json_util.default(o)
-    
+
 router = APIRouter()
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+def determine_sentiment_category(sentiment_scores):
+    """
+    Determine the sentiment category based on sentiment scores.
+    """
+    positive_score = sum(score['score'] for score in sentiment_scores if score['label'] in ['joy', 'surprise'])
+    negative_score = sum(score['score'] for score in sentiment_scores if score['label'] in ['sadness', 'anger', 'fear'])
+
+    logger.info(f"Positive Score: {positive_score}, Negative Score: {negative_score}")
+
+    if positive_score > negative_score:
+        return "positive"
+    elif negative_score > positive_score:
+        return "negative"
+    else:
+        return "neutral"
 
 @router.post(
     "/",
@@ -25,22 +43,21 @@ router = APIRouter()
 )
 async def create_journal_entry(journal_entry: JournalModel = Body(...)):
     sentiment_score = sentiment_analysis(journal_entry.entry)
-    # print(sentiment_score)
+    sentiment_category = determine_sentiment_category(sentiment_score)
+    
     try:
         result = await journal_collection.insert_one({
             "title": journal_entry.title,
             "entry": journal_entry.entry,
             "student_id": ObjectId(journal_entry.student_id),
             "sentiment_score": sentiment_score,
+            "sentiment_category": sentiment_category,
             "created_at": datetime.utcnow(),
             "updated_at": datetime.utcnow()
-            
         })
         new_journal_entry = await journal_collection.find_one({"_id": ObjectId(result.inserted_id)})
         new_journal_entry["_id"] = str(new_journal_entry["_id"])
-        
-       
-        
+
         student = await student_collection.find_one({"_id": new_journal_entry["student_id"]})
         if student:
             student["_id"] = str(student["_id"])
@@ -49,8 +66,9 @@ async def create_journal_entry(journal_entry: JournalModel = Body(...)):
             del new_journal_entry["student_id"]
     except Exception as e:
         raise HTTPException(status_code=400, detail="An error occurred while creating the journal entry.")
-    # return {"status: ", "success"}
+
     return new_journal_entry
+
 
 @router.get(
     "/",
@@ -60,19 +78,21 @@ async def create_journal_entry(journal_entry: JournalModel = Body(...)):
 async def list_journal_entries():
     journal_entries = await journal_collection.find().to_list(1000)
     
-    
     for journal_entry in journal_entries:
         journal_entry["_id"] = str(journal_entry["_id"])
-       
-          # Fetch the student details
+        
         student = await student_collection.find_one({"_id": ObjectId(journal_entry["student_id"])})
         if student:
             student["_id"] = str(student["_id"])
-            del student["password"]  # remove the password before adding the student details
+            del student["password"]
             journal_entry["student_details"] = student
         del journal_entry["student_id"]
-    return journal_entries
+        
+        # Determine sentiment category
+        sentiment_category = determine_sentiment_category(journal_entry["sentiment_score"])
+        journal_entry["sentiment_category"] = sentiment_category
 
+    return journal_entries
 
 @router.get("/emotions", response_description="Get students with positive emotions")
 async def get_students_with_positive_emotions():
@@ -103,7 +123,6 @@ async def get_students_with_positive_emotions():
 @router.get(
     "/{id}",
     response_description="Get a single journal entry",
-    # response_model=JournalModel,
     response_model_by_alias=False,
 )
 async def show_journal_entry(id: str):
@@ -114,15 +133,17 @@ async def show_journal_entry(id: str):
         student = await student_collection.find_one({"_id": ObjectId(journal_entry["student_id"])})
         if student:
             student["_id"] = str(student["_id"])
-            del student["password"]  # remove the password before adding the student details
+            del student["password"]
             journal_entry["student_details"] = student
         del journal_entry["student_id"]
+        
+        # Determine sentiment category
+        sentiment_category = determine_sentiment_category(journal_entry["sentiment_score"])
+        journal_entry["sentiment_category"] = sentiment_category
         
         return journal_entry
 
     raise HTTPException(status_code=404, detail=f"Journal entry {id} not found")
-
-
 
 @router.get("/student/{student_id}", response_description="Get journal entries by student ID")
 async def get_journals_by_student_id(student_id: str):
@@ -132,12 +153,15 @@ async def get_journals_by_student_id(student_id: str):
         student = await student_collection.find_one({"_id": ObjectId(journal_entry["student_id"])})
         if student:
             student["_id"] = str(student["_id"])
-            del student["password"]  # remove the password before adding the student details
+            del student["password"]
             journal_entry["student_details"] = student
         del journal_entry["student_id"] 
         
+        # Determine sentiment category
+        sentiment_category = determine_sentiment_category(journal_entry["sentiment_score"])
+        journal_entry["sentiment_category"] = sentiment_category
+        
     return journal_entries
-
 
 @router.patch("/{id}",
     response_description="Update a journal entry (only the title and entry can be updated)",
@@ -146,6 +170,7 @@ async def get_journals_by_student_id(student_id: str):
 async def update_journal(id: str, journal_entry: JournalModel = Body(...)):
     
     sentiment_score = sentiment_analysis(journal_entry.entry)
+    sentiment_category = determine_sentiment_category(sentiment_score)
     
     update_result = await journal_collection.find_one_and_update(
         {"_id": ObjectId(id)},
@@ -153,15 +178,18 @@ async def update_journal(id: str, journal_entry: JournalModel = Body(...)):
             "entry": journal_entry.entry,
             "title": journal_entry.title,
             "sentiment_score": sentiment_score,
-            "updated_at": datetime.utcnow(),   
+            "sentiment_category": sentiment_category,
+            "updated_at": datetime.utcnow(),
             }
          },
         return_document=ReturnDocument.AFTER,
     )
     if update_result is not None:
-        print("update_result", update_result)
         update_result["_id"] = str(update_result["_id"])
         update_result["student_id"] = str(update_result["student_id"])
+        
+        # Update sentiment category
+        update_result["sentiment_category"] = sentiment_category 
         return update_result
 
     if (existing_journal_entry := await journal_collection.find_one({"_id": ObjectId(id)})) is not None:
--- app/services/sentiment_analysis.py
+++ app/services/sentiment_analysis.py
@@ -1,10 +1,17 @@
 from transformers import RobertaTokenizerFast, TFRobertaForSequenceClassification, pipeline
 
-tokenizer = RobertaTokenizerFast.from_pretrained("arpanghoshal/EmoRoBERTa")
-model = TFRobertaForSequenceClassification.from_pretrained("arpanghoshal/EmoRoBERTa")
+hf_token = "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"
+
+model_name = "arpanghoshal/EmoRoBERTa"
+
+tokenizer = RobertaTokenizerFast.from_pretrained(model_name, token=hf_token)
+model = TFRobertaForSequenceClassification.from_pretrained(model_name, token=hf_token)
 
 emotion = pipeline('sentiment-analysis', 
-                    model='arpanghoshal/EmoRoBERTa', return_all_scores=True, tokenizer=tokenizer)
+                   model=model_name, 
+                   return_all_scores=True, 
+                   tokenizer=tokenizer,
+                   token=hf_token)
 
 
 def sentiment_analysis(text: str):
